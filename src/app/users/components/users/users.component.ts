e3befaf7aee18cd4c558d202ca38ff2e5e98bd6b
aya-sakr <ayasakr1000@gmail.com>
2025-07-18 10:11:42 +0300

    enhancement in angular material table in user commponent

--- src/app/users/components/users/users.component.ts
+++ src/app/users/components/users/users.component.ts
@@ -1,17 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { UsersService } from '../../Service/users.service';
 import { Iusers } from 'src/app/Models/iusers';
 import { MatDialog } from '@angular/material/dialog';
 import { AddUsersComponent } from '../add-users/add-users.component';
 import { SharedUserService } from '../../Service/shared-user.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.scss'],
 })
-export class UsersComponent implements OnInit {
-  allUsers: Iusers[] = [];
+export class UsersComponent implements OnInit{
+   @ViewChild(MatPaginator) paginator!: MatPaginator;
+    dataSource:any;  
+    displayedColumns: string[] = ['index', 'username', 'password','action'];
+
+
+  
+
   constructor(
     private userSharedService: SharedUserService,
     private usersService: UsersService,
@@ -24,16 +32,20 @@ export class UsersComponent implements OnInit {
 
     this.userSharedService.getNewUser().subscribe((res: any) => {
       if (res) {
-        this.allUsers.push(res);
+        this.dataSource.push(res);
         this.getUsers();
       }
     });
   }
 
   getUsers() {
-    this.usersService.getAllUsers().subscribe((data: Iusers[]) => {
-      this.allUsers = data;
+    this.usersService.getAllUsers().subscribe((users: Iusers[]) => {
+       this.dataSource = new MatTableDataSource(users);
+       this.dataSource.paginator = this.paginator;
     });
+
+  
+
   }
   addNewUser() {
     this.dialog.open(AddUsersComponent, {
@@ -54,10 +66,10 @@ export class UsersComponent implements OnInit {
         this.getUsers();
       });
   }
-  deletUser(userId: any, userIndex: any) {
-    this.allUsers.splice(userIndex, 1);
-    this.usersService.deletUser(userId).subscribe(() => {
+  deleteUser(userId: any, userIndex: any) {
+      this.usersService.deletUser(userId).subscribe(() => {
       this.toaster.success('The product deleted ', 'success');
+      this.getUsers()
     });
 
   
